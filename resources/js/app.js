512a439f4d0d60e19ffc3b3cfd1a0c69635df220
Azhar Azeez <107195591+azhar-azeez@users.noreply.github.com>
2024-05-11 06:18:16 +0530

    Installed chartjs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import './bootstrap';
 import 'laravel-datatables-vite';
+import Chart from 'chart.js/auto';
 // import './../../vendor/power-components/livewire-powergrid/dist/powergrid'
 // import './../../vendor/power-components/livewire-powergrid/dist/bootstrap5.css'
